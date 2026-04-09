682279249f8db1498ddfb9531d1b56ffc64cd5b2
Reycamilo <josecamilo2404@gmail.com>
2026-04-09 08:21:09 -0600

    Agregando archivo Prueba

---
+++ PersonasApp/Pruebas/Prueba.cs
@@ -0,0 +1,10 @@
+namespace PersonasApp.Pruebas
+{
+    public class Prueba
+    {
+        public static void Ejecutar()
+        {
+            Console.WriteLine("esto es un saludol");
+        }
+    }
+}
